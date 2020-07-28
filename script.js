095871d38e92c96a20ed50f9c5516cc2a4f2a209
Sasha Cindemir <68419738+scindemir@users.noreply.github.com>
2020-07-29 00:22:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const bb = document.getElementById("buttonBay");
+var btn1 = document.createElement("button");
+const text = document.createTextNode("button1");
+btn1.appendChild(text);
+btn1.innerHTML = "CLEAR";                   
+document.body.appendChild(btn1);
+
+$(btn1).click(function(){
+    $(".gridItem").css("background-color", "white");
+})
+
+
 const container = document.getElementById("container");
 
 function makeRows(rows, cols) {
@@ -16,10 +28,20 @@ $(".gridItem").mouseover(function(){
     $(this).css("background-color", "black");
 })
 
-var btn = document.createElement("button");   
-btn.innerHTML = "CLEAR";                   
-document.body.appendChild(btn);
 
-$("button").click(function(){
-    $(".gridItem").css("background-color", "white");
-})
+var btn2 = document.createElement("button");
+const text2 = document.createTextNode("button2");
+btn2.appendChild(text);
+btn2.innerHTML = "SET CUSTOM LENGTH";                   
+document.body.appendChild(btn2);
+
+$(btn2).click(function enterNum(){
+             let amount = window.prompt("Please enter the number", "");
+
+                if (amount == null || amount == "") {
+                   return false;
+                } else {
+                  $("#container").empty();
+                  makeRows(amount, amount);
+                
+}})
